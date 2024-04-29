f34844abc2dfd2327947d41ca9affa39570f87c9
asifahamed11 <asif223311112@outlook.com>
2024-04-29 18:00:32 +0600

    test2

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App extends JFrame {
     public static void main(String[] args) {
         App frame = new App();
         frame.setVisible(true);
+        frame.setBounds(500, 150, 300, 500);
     }
 }
